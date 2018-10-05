22214fd59faaf9fc04785b7114e97b4fb7fde921
Karoczkai Krisztián <krisztian@mozilla.hu>
2018-10-05 15:19:57 +0200

    Hero JDBC

---
+++ hero/hero-service-object/src/main/java/hu/javanetacademy/hoe/hero/service/object/HeroService.java
@@ -0,0 +1,27 @@
+package hu.javanetacademy.hoe.hero.service.object;
+
+import hu.javanetacademy.hoe.hero.dao.file.HeroJDBCDAOImpl;
+import hu.javanetacademy.hoe.hero.dao.model.Hero;
+import hu.javanetacademy.hoe.hero.dao.model.HeroDAOInterface;
+
+import java.util.List;
+
+/**
+ * @author krisztian
+ */
+public class HeroService {
+   private HeroDAOInterface dao= new HeroJDBCDAOImpl();
+   
+   public Hero create(Hero pNewHero){
+       Hero avb = dao.getByNameFromUser(pNewHero.getName(), pNewHero.getUserid());
+       if(avb==null)
+        return dao.create(pNewHero);
+       else 
+           throw new CustomException();
+   }
+   
+   public List<Hero> getHeroByUser(long pUserId){
+       return dao.getByUser(pUserId);
+   }
+   
+}
