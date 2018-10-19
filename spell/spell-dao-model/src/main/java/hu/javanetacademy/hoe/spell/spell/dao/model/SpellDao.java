60d82f4bb843b0866887f2d3ea49b5864e82b532
maxxysfm <maxxywarrior@gmail.com>
2018-10-19 14:30:16 +0200

    Test

---
+++ spell/spell-dao-model/src/main/java/hu/javanetacademy/hoe/spell/spell/dao/model/SpellDao.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.javanetacademy.hoe.spell.spell.dao.model;
+
+/**
+ *
+ * @author Attila
+ */
+public interface SpellDao {
+    
+}
