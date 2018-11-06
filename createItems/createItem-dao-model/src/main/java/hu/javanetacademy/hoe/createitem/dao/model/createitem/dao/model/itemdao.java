984869b4b2aa5386d61ac0834e331aa5d9c0338f
Nagy Ádám <nagy.adam0901@gmail.com>
2018-11-06 16:59:51 +0100

    update, delet code

--- createItems/createItem-dao-model/src/main/java/hu/javanetacademy/hoe/createitem/dao/model/createitem/dao/model/itemdao.java
+++ createItems/createItem-dao-model/src/main/java/hu/javanetacademy/hoe/createitem/dao/model/createitem/dao/model/itemdao.java
@@ -1,28 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hu.javanetacademy.hoe.createitem.dao.model.createitem.dao.model;
-
 import java.util.List;
-
 /**
- *
- * @author Nagy Adam
-<<<<<<< HEAD
  * 
+ * @author Nagy Adam
  */
-//public interface itemdao {
-    
-//=======
-// */
 public interface itemdao {
-//>>>>>>> origin/master
     public List<item> getAll();
     public item getById(long id);
     public item getByName(String name);
     public item modify(long id,long mennyiseg);
     public item delete(long id);
-
 }
